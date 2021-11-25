10d2cde9bcfd87d74d88d25d9f34c40d2913f552
JohnsonMori <960612296@qq.com>
2021-11-25 20:56:11 +0800

    wx-request swiper data

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,38 +1,20 @@
 //Page Object
 Page({
   data: {
-
+    // 轮播图数组
+    swiperList: []
   },
-  //options(Object)
+  // 页面开始加载 就会触发
   onLoad: function (options) {
-
-  },
-  onReady: function () {
-
-  },
-  onShow: function () {
-
-  },
-  onHide: function () {
-
-  },
-  onUnload: function () {
-
-  },
-  onPullDownRefresh: function () {
-
-  },
-  onReachBottom: function () {
-
-  },
-  onShareAppMessage: function () {
-
-  },
-  onPageScroll: function () {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap: function (item) {
+    // 1 发送异步请求获取轮播图数据
+    var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result) => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      }
+    });
 
   }
 });
