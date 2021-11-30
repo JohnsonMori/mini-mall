3d59b2f0498f745df35f36e7d73921d6e4de376a
JohnsonMori <960612296@qq.com>
2021-11-30 21:51:54 +0800

    goods list search and tabs component

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -5,7 +5,23 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        tabs: [
+            {
+                id: 0,
+                value: "综合",
+                isActive: true
+            },
+            {
+                id: 1,
+                value: "销量",
+                isActive: false
+            },
+            {
+                id: 2,
+                value: "价格",
+                isActive: false
+            }
+        ]
     },
 
     /**
@@ -14,53 +30,8 @@ Page({
     onLoad: function (options) {
         console.log(options);
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    handleTabsItemChange(e) {
+        const tabs = this.data.tabs.map((v, i) => { return { ...v, isActive: i === e.detail.index } })
+        this.setData({ tabs })
     }
 })
