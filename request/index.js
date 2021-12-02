694f2b400691ec466c5e92ab025d597b27cfb232
JohnsonMori <960612296@qq.com>
2021-12-02 15:08:02 +0800

    global loading

--- request/index.js
+++ request/index.js
@@ -1,4 +1,14 @@
+// 同时发送异步请求代码的次数
+let ajaxTimes = 0;
 export const request = (params) => {
+  ajaxTimes++;
+  // 显示加载中 效果
+  wx.showLoading({
+    title: "加载中",
+    mask: true,
+  });
+
+
   // 定义公共的url
   const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
   return new Promise((resolve, reject) => {
@@ -10,8 +20,14 @@ export const request = (params) => {
       },
       fail: (err) => {
         reject(err);
+      },
+      complete: () => {
+        ajaxTimes--;
+        if (ajaxTimes === 0) {
+          // 关闭正在等待的图标
+          wx.hideLoading();
+        }
       }
     });
-
   })
 }
