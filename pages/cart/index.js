ba4afdf433b652ba2ac74d858cd7c8df2ab2ca81
JohnsonMori <960612296@qq.com>
2021-12-02 21:32:44 +0800

    choose address function

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -2,10 +2,29 @@
 Page({
     // 点击 收货地址
     handleChooseAddress() {
-        wx.chooseAddress({
+        wx.getSetting({
             success: (result) => {
-                console.log(result);
-            }
+                const scopeAddress = result.authSetting["scope.address"];
+                if (scopeAddress === true || scopeAddress === undefined) {
+                    wx.chooseAddress({
+                        success: (result1) => {
+                            console.log(result1);
+                        }
+                    });
+                } else {
+                    wx.openSetting({
+                        success: () => {
+                            wx.chooseAddress({
+                                success: (result3) => {
+                                    console.log(result3);
+                                }
+                            });
+                        }
+                    });
+                }
+            },
+            fail: () => { },
+            complete: () => { }
         });
     }
 })
