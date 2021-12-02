4e37e7cd4927d562ef158b3c01d7cf1b734030d5
JohnsonMori <960612296@qq.com>
2021-12-02 16:02:27 +0800

    goods detail request data

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,26 @@
-// pages/goods_detail/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime.js";
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        goodsObj: {}
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        const { goods_id } = options;
+        this.getGoodsDetail(goods_id);
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    // 获取商品详情数据
+    async getGoodsDetail(goods_id) {
+        const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
+        this.setData({
+            goodsObj
+        })
     }
 })
