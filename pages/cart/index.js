f34905eacfd67605d5540b3a46caaeaf930b53b8
JohnsonMori <960612296@qq.com>
2021-12-03 16:29:50 +0800

    cart data all checked

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -3,12 +3,15 @@ import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
     data: {
         address: {},
-        cart: []
+        cart: [],
+        allChecked: false
     },
     onShow() {
         const address = wx.getStorageSync("address");
-        const cart = wx.getStorageSync("cart");
-        this.setData({ address, cart });
+        const cart = wx.getStorageSync("cart") || [];
+        // 空数组 调用 every，返回值就是true
+        const allChecked = cart.length ? cart.every(v => v.checked) : false;
+        this.setData({ address, cart, allChecked });
     },
     // 点击 收货地址
     async handleChooseAddress() {
