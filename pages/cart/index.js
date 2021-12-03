7dd020dfcf261fb958921f2ecf8b560a3f34c49f
JohnsonMori <960612296@qq.com>
2021-12-03 16:42:36 +0800

    cart total price and total number

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -4,14 +4,28 @@ Page({
     data: {
         address: {},
         cart: [],
-        allChecked: false
+        allChecked: false,
+        totalPrice: 0,
+        totalNum: 0
     },
     onShow() {
         const address = wx.getStorageSync("address");
         const cart = wx.getStorageSync("cart") || [];
         // 空数组 调用 every，返回值就是true
-        const allChecked = cart.length ? cart.every(v => v.checked) : false;
-        this.setData({ address, cart, allChecked });
+        // const allChecked = cart.length ? cart.every(v => v.checked) : false;
+        let allChecked = true;
+        let totalPrice = 0;
+        let totalNum = 0;
+        cart.forEach(v => {
+            if (v.checked) {
+                totalPrice += v.num * v.goods_price;
+                totalNum += v.num;
+            } else {
+                allChecked = false;
+            }
+        });
+        allChecked = cart.length !== 0 ? allChecked : false;
+        this.setData({ address, cart, allChecked, totalPrice, totalNum });
     },
     // 点击 收货地址
     async handleChooseAddress() {
