99d091588514a34c820119a1ad7a1bc9e0b4ef39
JohnsonMori <960612296@qq.com>
2021-12-04 09:52:53 +0800

    pay page render

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,23 @@
-// pages/pay/index.js
+import { getSetting, chooseAddress, openSetting, showModal, showToast } from '../../utils/asyncWx.js';
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        address: {},
+        cart: [],
+        totalPrice: 0,
+        totalNum: 0
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    onShow() {
+        const address = wx.getStorageSync("address");
+        let cart = wx.getStorageSync("cart") || [];
+        cart = cart.filter(v => v.checked);
+        this.setData({ address });
+        let totalPrice = 0;
+        let totalNum = 0;
+        cart.forEach(v => {
+            totalPrice += v.num * v.goods_price;
+            totalNum += v.num;
+        });
+        this.setData({ cart, totalPrice, totalNum, address });
     }
 })
