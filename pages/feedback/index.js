7d9acba29e92f58cda03a2d13dbbd6af00c01234
JohnsonMori <960612296@qq.com>
2021-12-05 19:47:20 +0800

    feedback choose and display image

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -13,10 +13,26 @@ Page({
                 value: "商品、商家投诉",
                 isActive: false
             }
-        ]
+        ],
+        // 被选中的图片路径 数组
+        chooseImgs: []
     },
     handleTabsItemChange(e) {
-        const tabs = this.data.tabs.map((v, i) => { return { ...v, isActive: i === e.detail.index } })
-        this.setData({ tabs })
+        const tabs = this.data.tabs.map((v, i) => { return { ...v, isActive: i === e.detail.index } });
+        this.setData({ tabs });
+    },
+    // 点击 “+” 选择图片
+    handleChooseImg() {
+        wx.chooseImage({
+            count: 9,
+            sizeType: ['original', 'compressed'],
+            sourceType: ['album', 'camera'],
+            success: (result) => {
+                this.setData({
+                    chooseImgs: [...this.data.chooseImgs, ...result.tempFilePaths]
+                })
+            }
+        });
+
     }
 })
