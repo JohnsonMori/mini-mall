7a3856b1021552990f693dc12b126190de466cc5
JohnsonMori <960612296@qq.com>
2021-12-05 14:43:09 +0800

    feedback tabs

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,66 +1,22 @@
-// pages/feedback/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime.js";
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        tabs: [
+            {
+                id: 0,
+                value: "体验问题",
+                isActive: true
+            },
+            {
+                id: 1,
+                value: "商品、商家投诉",
+                isActive: false
+            }
+        ]
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    handleTabsItemChange(e) {
+        const tabs = this.data.tabs.map((v, i) => { return { ...v, isActive: i === e.detail.index } })
+        this.setData({ tabs })
     }
 })
