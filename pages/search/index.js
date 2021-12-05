6f26880f42eeb300c2f7524c5d792b354a917041
JohnsonMori <960612296@qq.com>
2021-12-05 14:13:34 +0800

    search center render result

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,22 @@
-// pages/search/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime.js";
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        goods: []
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    // 输入框的值改变 就会触发的事件
+    handleInput(e) {
+        const { value } = e.detail;
+        if (!value.trim()) {
+            return;
+        }
+        this.qsearch(value);
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    // 发送请求获取搜索建议 数据
+    async qsearch(query) {
+        const res = await request({ url: "/goods/qsearch", data: { query } });
+        this.setData({
+            goods: res
+        })
     }
 })
