6bea4b82309b2c62f5d52466bc7b54440b13c504
JohnsonMori <960612296@qq.com>
2021-12-05 10:51:26 +0800

    request order data

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,10 +1,12 @@
-// pages/order/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime.js";
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
+        orders: [],
         tabs: [
             {
                 id: 0,
@@ -28,63 +30,29 @@ Page({
             }
         ]
     },
+    onShow() {
+        const token = wx.getStorageSync("token");
+        if (!token) {
+            wx.navigateTo({
+                url: '/pages/auth/index',
+            });
+            return;
+        }
+
+        let pages = getCurrentPages();
+        let currentPage = pages[pages.length - 1];
+        const { type } = currentPage.options;
+        this.getOrders(type);
+    },
+    // 获取订单列表的方法
+    async getOrders(type) {
+        const res = await request({ url: "/my/orders/all", data: { type } });
+        this.setData({
+            orders: res.orders
+        })
+    },
     handleTabsItemChange(e) {
         const tabs = this.data.tabs.map((v, i) => { return { ...v, isActive: i === e.detail.index } })
         this.setData({ tabs })
-    },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
     }
 })
